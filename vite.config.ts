383b49044318a6d41c5921a717992c5a42fe43d5
Anas0019-sy <f14001995@gmail.com>
2025-07-28 18:50:02 +0300

    Fix: correct base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPO_NAME/', // <--- هذا هو السطر الوحيد الذي ستضيفه
+  // تأكد من أن هذا السطر يطابق اسم مستودعك تمامًا
+  base: '/coresoftSchoole/',
 })
